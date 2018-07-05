8cce8557eda984a9ba3ed865460d72a55f55215b
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-07-05 15:41:49 +0100

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,43 @@ else
     using Test
 end
 
-# write your own tests here
-@test 1 == 1
+@testset "utils" begin
+    d = Widget{:test}(Dict(:a => 1, :b => Observable(2)))
+    m = Widgets.@map d :a + :b[]
+    n = Widgets.@map d :a + $(:b)
+    @test m == 3
+    @test n[] == 3
+    d[:b][] = 3
+    sleep(0.1)
+    @test m == 3
+    @test n[] == 4
+
+    l = Widgets.@layout :a + :b[]
+    @test l(d) == 4
+end
+
+@testset "widget" begin
+    @widget function myui(x)
+        :a = x + 1
+        :b = Observable(10)
+        _.output = $(:b) + :a
+        _.display = "The sum is "*string($(_.output))
+        _.layout = _.display
+    end
+    ui = myui(5)
+    @test ui[:a] == 6
+    @test ui[:b][] == 10
+    @test ui.output[] == 16
+    @test ui.display[] == ui.layout(ui)[] == "The sum is 16"
+
+    ui = Widgets.widget(Val(:myui), 5)
+    @test ui[:a] == 6
+    @test ui[:b][] == 10
+    @test ui.output[] == 16
+    @test ui.display[] == ui.layout(ui)[] == "The sum is 16"
+
+    ui[:b][] = 11
+    sleep(0.1)
+    @test ui.output[] == 17
+    @test ui.display[] == ui.layout(ui)[] == "The sum is 17"
+end
