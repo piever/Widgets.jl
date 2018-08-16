0136c8577a3360908ee91473300563741a5a6e77
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-08-16 20:04:42 +0100

    remove widget macro

--- src/widget.jl
+++ src/widget.jl
@@ -96,57 +96,6 @@ Base.getindex(ui::Widget, i::AbstractString) = getindex(ui, Symbol(i))
 Base.setindex!(ui::Widget, val, i::Symbol) = setindex!(components(ui), val, i)
 Base.setindex!(ui::Widget, val, i::AbstractString) = setindex!(ui, val, Symbol(i))
 
-replace_ref(s) = s
-
-replace_ref(d, x...) = foldl((a,b) -> Expr(:ref, a, b), x, init = d)
-
-"""
-`@widget(wdgname, func_call)`
-
-Special macro to create "recipes" for custom widgets. The `@widget` macro takes to argument, a variable
-name `wdgname` and a function call `func_call`. The function call is changed by the macro in several ways:
-- an extra line is added at the beginning to initiliaze a variable called `wdgname::Widget` that can be used to refer to the widget in the function body
-- all lines of the type `sym::Symbol = expr` are replaced with `wdgname[sym] = @map(wdgname, expr)`, see [`Widgets.@map`](@ref) for more details
-- an extra line is added at the end to return `wdgname`
-
-The macro then registers the function `func_call` and exports it.
-It also overloads the `widget` function with the following signature:
-
-`Widgets.widget(::Val{Symbol(func_name)}, args...; kwargs..) = func_name(args...; kwargs...)`
-"""
-macro widget(args...)
-    Base.depwarn("Widgets.@widget is deprecated", "widget")
-    @assert 1 <= length(args) <= 2
-    func_call = args[end]
-    d = length(args) == 2 ? args[1] : gensym(:widget)
-    func_call.head == :function || error("@widget accepts only function definitions")
-    func_signature, func_body = func_call.args
-    func_name = func_signature.args[1]
-    @assert func_body.head == :block
-    v = func_body.args
-    for (i, line) in enumerate(v)
-        if extract_name(line) == Symbol("@auto")
-            expr = auto_helper!(line.args[2], wrap = true)
-            line.head = expr.head
-            line.args = expr.args
-        end
-        if iswidgetassignment(line)
-            line.args[2] = map_helper(d, line.args[2])
-            line.args[1] = parse_function_call(d, line.args[1], replace_ref)
-        end
-    end
-    shortname = extract_name(func_name)
-    qn = quotenode(shortname)
-    pushfirst!(v, :($d = Widgets.Widget{$qn}()))
-    push!(v, d)
-    (shortname == :widget) && return esc(:(Base.@__doc__ $func_call))
-    quote
-        Base.@__doc__ $func_call
-        Widgets.widget(::Val{$qn}, args...; kwargs...) = $func_name(args...; kwargs...)
-        export $func_name
-    end |> esc
-end
-
 """
 `@auto(expr)`
 
