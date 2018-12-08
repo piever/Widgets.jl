90f0c6fa5c62e0545b929f4cddec578f0fa51b99
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-12-08 19:31:12 +0000

    remove file

--- src/Widgets.jl
+++ src/Widgets.jl
@@ -9,7 +9,6 @@ export Widget, widget, @layout!, node, scope, scope!
 include("widget.jl")
 include("utils.jl")
 include("layout.jl")
-include("widget_placeholder.jl")
 include("backend.jl")
 include("defaults.jl")
 include("manipulate.jl")
