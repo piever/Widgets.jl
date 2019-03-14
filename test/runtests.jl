63ff85fc853be98a95b8389e10eb497e2ea6186c
Pietro Vertechi <pietro.vertechi@protonmail.com>
2019-03-14 15:04:11 +0000

    test manipulate (#30)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using Widgets, Observables, OrderedCollections, InteractBase, WebIO
+using Colors
 using Widgets: Widget, @layout, widgettype
 import Widgets: observe, @auto
 using Test
@@ -99,3 +100,32 @@ end
     @test t[] == 12
     @test s[] == 11
 end
+
+@testset "manipulate" begin
+    ui = @manipulate for r = 0:.05:1, g = 0:.05:1, b = 0:.05:1
+        RGB(r,g,b)
+    end
+    @test observe(ui)[] == RGB(0.5, 0.5, 0.5)
+    observe(ui, :r)[] = 0.1
+    sleep(0.1)
+    @test observe(ui)[] == RGB(0.1, 0.5, 0.5)
+    ui = @manipulate throttle = 1 for r = 0:.05:1, g = 0:.05:1, b = 0:.05:1
+        RGB(r,g,b)
+    end
+    observe(ui, :r)[] = 0.1
+    sleep(0.1)
+    observe(ui, :r)[] = 0.3
+    sleep(0.1)
+    observe(ui, :g)[] = 0.1
+    sleep(0.1)
+    observe(ui, :g)[] = 0.3
+    sleep(0.1)
+    observe(ui, :b)[] = 0.1
+    sleep(0.1)
+    observe(ui, :b)[] = 0.3
+    sleep(0.1)
+    @test observe(ui)[] != RGB(0.3, 0.3, 0.3)
+    sleep(1.5)
+    @test observe(ui)[] == RGB(0.3, 0.3, 0.3)
+end
+
